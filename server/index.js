9cdb7a77c188d71edd6698f47bdb094c245a6fbd
s3782490 <s3782490@student.rmit.edu.au>
2022-07-04 14:25:22 +1000

    Added router import

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const db = require('./db')
+const db = require('./db');
+const movieRouter = require('./routes/movie-router');
 const app = express();
 const apiPort = 3000;
 
@@ -14,5 +15,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
     res.send('Hello world!');
 })
+
+app.use('/api', movieRouter);
 
 app.listen(apiPort, () => console.log(`Server is running on port ${apiPort}`))
