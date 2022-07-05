1327abbdf0936937827531bc7d30a5991c5296f7
s3782490 <s3782490@student.rmit.edu.au>
2022-07-05 14:10:14 +1000

    Basic movie listing with axios request

--- client/src/pages/MoviesList.jsx
+++ client/src/pages/MoviesList.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import { React, useEffect, useState } from 'react'
+import api from '../api'
+import styled from 'styled-components'
+
+const Wrapper = styled.div.attrs({
+    className: 'p-5',
+})``
 
 export default function MoviesList() {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setLoading] = useState(false);
+
+    useEffect(() => {
+        console.clear()
+        const fetchMovies = async () => {
+            setLoading(true);
+            await api.getAllMovies().then(movies => {
+                setMovies(movies.data.data);
+                setLoading(false);
+            })
+        }
+        fetchMovies();
+    }, [])
+
+    let showTable = true
+    if (!movies.length) {
+        showTable = false
+    }
+
     return (
-        <React.Fragment>
-            <p>List of movies will go here</p>
-        </React.Fragment>
+        <Wrapper>
+            {isLoading ? 'waiting' : movies.map(movie => (
+            <p>{movie.name}</p>
+        ))}
+        </Wrapper>
     );
 }
