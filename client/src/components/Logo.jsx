014b5b1c9f90b8631eb332c6440e713ed46662c2
s3782490 <s3782490@student.rmit.edu.au>
2022-07-05 11:24:35 +1000

    Changed divs to React fragments

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -4,10 +4,10 @@ import logo from '../logo.svg'
 
 export default function Logo() {
     return (
-        <div>
+        <React.Fragment>
             <a className="navbar-brand" href="https://rmit.instructure.com/">
                 <img src={logo} width="50" height="50" />
             </a>
-        </div>
+        </React.Fragment>
     );
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,13 +5,13 @@ import Links from "./Links";
 
 export default function NavBar() {
     return (
-        <div>
+        <React.Fragment>
             <div className="container">
                 <div className="navbar navbar-expand-lg navbar-dark bg-dark mb-20">
                     <Logo />
                     <Links />
                 </div>
             </div>
-        </div>
+        </React.Fragment>
     );
 }
