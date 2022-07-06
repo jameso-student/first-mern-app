7855b03f21c663241cb721b4f2726eb8eeadc9e8
s3782490 <s3782490@student.rmit.edu.au>
2022-07-06 16:02:23 +1000

    Added movie deletion and update link

--- client/src/pages/MoviesList.jsx
+++ client/src/pages/MoviesList.jsx
@@ -6,12 +6,46 @@ const Wrapper = styled.div.attrs({
     className: 'p-5',
 })``
 
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor: pointer;
+`
+
+const Delete = styled.div`
+    color: #ff0000;
+    cursor: pointer;
+`
+
+function UpdateMovie(props) {
+    const updateUser = event => {
+        event.preventDefault();
+        window.location.href = `/movies/update/${props.id}`;
+    }
+
+    return <Update onClick={updateUser}>Update</Update>
+}
+
+function DeleteMovie(props) {
+    const deleteMovie = event => {
+        event.preventDefault();
+
+        if (window.confirm(
+            `Do you want to delete the movie ${props.name} permanently?`
+            )
+        ) {
+            api.deleteMovieById(props.id);
+            window.location.reload();
+        }
+    }
+
+    return <Delete onClick={deleteMovie}>Delete</Delete>
+}
+
 export default function MoviesList() {
     const [movies, setMovies] = useState([]);
     const [isLoading, setLoading] = useState(false);
 
     useEffect(() => {
-        console.clear()
         const fetchMovies = async () => {
             setLoading(true);
             await api.getAllMovies().then(movies => {
@@ -26,20 +60,26 @@ export default function MoviesList() {
         <Wrapper>
             {isLoading ? 'Loading...' : 
                 <table className='table'>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Rating</th>
-                        <th>Time</th>
-                    </tr>
-                    {movies.map(movie => (
-                    <tr>
-                        <td>{movie._id}</td>
-                        <td>{movie.name}</td>
-                        <td>{movie.rating}</td>
-                        <td>{movie.time.join(' / ')}</td>
-                    </tr>
-                    ))}
+                    <tbody>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Rating</th>
+                            <th>Time</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                        {movies.map(movie => (
+                        <tr key={movie._id}>
+                            <td>{movie._id}</td>
+                            <td>{movie.name}</td>
+                            <td>{movie.rating}</td>
+                            <td>{movie.time.join(' / ')}</td>
+                            <td>{<UpdateMovie id={movie._id} name={movie.name} />}</td>
+                            <td>{<DeleteMovie id={movie._id} name={movie.name} />}</td>
+                        </tr>
+                        ))}
+                    </tbody>
                 </table>
             }
         </Wrapper>
